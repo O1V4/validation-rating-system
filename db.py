23e94167061bb82568f6a418ec2cf4d979088210
Oiva <72695556+oivaaa@users.noreply.github.com>
2023-10-22 00:09:42 +0300

    stopped using SQLAlchemyn db.model classes due to ohjaaja-feedback

--- db.py
+++ db.py
@@ -1,24 +1,7 @@
+from app import app
+from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 
 
-db = SQLAlchemy()
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200), unique=True, nullable=False)
-    password = db.Column(db.String(230), nullable=False)
-
-class Submission(db.Model):
-    __tablename__ = 'submissions'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    ratings = db.relationship('Rating', backref='submission', lazy=True)
-
-class Rating(db.Model):
-    __tablename__ = 'ratings'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    submission_id = db.Column(db.Integer, db.ForeignKey('submissions.id'), nullable=False)
-    rating_value = db.Column(db.Integer, nullable=False)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
