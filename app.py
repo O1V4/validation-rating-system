5575d4afc73670d8df392eb6becb16dc0d04d99e
Oiva <72695556+oivaaa@users.noreply.github.com>
2023-10-22 00:18:42 +0300

    structural changes to the app

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 """Modules providing Flask, secret key import and db."""
 from os import getenv
 from flask import Flask
-from db import db
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.secret_key = getenv("SECRET_KEY")
-db.init_app(app)
+
 
 import routes
